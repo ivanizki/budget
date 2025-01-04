ea4c4e824824258bf563db792c232034d56e3a93
Dmitry Ivanizki <dmitry.ivanizki@gmail.com>
2025-01-04 19:49:59 +0100

    Added attribute and column for Miete.

--- src/main/java/ivanizki/budget/WohnungTableConfigurationProvider.java
+++ src/main/java/ivanizki/budget/WohnungTableConfigurationProvider.java
@@ -11,22 +11,37 @@ import com.top_logic.layout.table.model.TableConfiguration;
  */
 public class WohnungTableConfigurationProvider extends NoDefaultColumnAdaption {
 
+	private static final int COLUMN_HEADER_LEFT_WIDTH = 15;
+
+	private static final int COLUMN_HEADER_LETTER_WIDTH = 5;
+
+	private static final int COLUMN_HEADER_RIGHT_WIDTH = 30;
+
 	@Override
 	public void adaptConfigurationTo(TableConfiguration table) {
-		table.getDeclaredColumn("aktiv").setDefaultColumnWidth("50px");
+		adaptWidth(table, "aktiv", "A", "Aktiv");
 		table.getDeclaredColumn("bezeichnung").setDefaultColumnWidth("250px");
 		table.getDeclaredColumn("adresse").setDefaultColumnWidth("250px");
-		adaptDeclaredCostColumn(table, "kaltmiete", "Kalt", "Kaltmiete");
-		adaptDeclaredCostColumn(table, "nebenkosten", "NK", "Nebenkosten");
-		table.getDeclaredColumn("zimmeranzahl").setDefaultColumnWidth("50px");
-		table.getDeclaredColumn("zzglHeizkosten").setDefaultColumnWidth("50px");
-		table.getDeclaredColumn("flaeche").setDefaultColumnWidth("75px");
-		table.getDeclaredColumn("energieklasse").setDefaultColumnWidth("75px");
+		adaptWidth(table, "miete", "Miete", "Miete", "75px");
+		adaptWidth(table, "kaltmiete", "Kalt", "Kaltmiete", "75px");
+		adaptWidth(table, "nebenkosten", "NK", "Nebenkosten", "75px");
+		adaptWidth(table, "zimmeranzahl", "Z", "Zimmeranzahl");
+		adaptWidth(table, "zzglHeizkosten", "+", "zzgl. Heizkosten");
+		adaptWidth(table, "flaeche", "F", "Fläche");
+		adaptWidth(table, "energieklasse", "E", "Energieklasse");
 	}
 
-	private void adaptDeclaredCostColumn(TableConfiguration table, String name, String label, String tooltip) {
+	private void adaptWidth(TableConfiguration table, String name, String label, String tooltip) {
+		String columnWidth = new StringBuilder()
+			.append(COLUMN_HEADER_LEFT_WIDTH + label.length() * COLUMN_HEADER_LETTER_WIDTH + COLUMN_HEADER_RIGHT_WIDTH)
+			.append("px")
+			.toString();
+		adaptWidth(table, name, label, tooltip, columnWidth);
+	}
+
+	private void adaptWidth(TableConfiguration table, String name, String label, String tooltip, String columnWidth) {
 		ColumnConfiguration costColumn = table.getDeclaredColumn(name);
-		costColumn.setDefaultColumnWidth("100px");
+		costColumn.setDefaultColumnWidth(columnWidth);
 		costColumn.setColumnLabel(label);
 		/** TODO add tooltip */
 	}
